76ff779d9e671be32b7733f457a09d60d15ef2ee
artursahak <58938408+artursahak@users.noreply.github.com>
2020-04-25 20:04:10 +0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Network Constants
+CROP_SIZE = 227
+CROP_PAD = 2
+MAX_TRACK_LENGTH = 32
+LSTM_SIZE = 512
+
+import os.path
+LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs')
+GPU_ID = '0'
+
+# Drawing constants
+OUTPUT_WIDTH = 1280
+OUTPUT_HEIGHT = 720
+PADDING = 2
